7b869b863aade9fcf93910fa56ec458ea1e8ab8a
Wilfried Woivre <wilfried.woivre@gmail.com>
2019-06-15 16:47:29 +0200

    init repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace labs_intro_azure_data.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
